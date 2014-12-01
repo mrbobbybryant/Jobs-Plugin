b8a56718b1d42834c6ec51d9c489c5dc20471d7b
Adam Silverstein <adam@10up.com>
2014-12-01 08:25:49 -0700

    Whitespace/code cleanup
    * add limits and no_count_rows to optimize queries
    * add some missing output escaping
    * change some spaces to tabs, some remain
    * add coding standards spacing to JS as well
    * cast post id as (int) to avoid XSS vector

--- hrm-jobs.php
+++ hrm-jobs.php
@@ -30,7 +30,7 @@ if ( is_object($screen) && 'job' == $screen->post_type ) {
 	wp_enqueue_script( 'reorder-js', plugins_url( '/js/reorder.js', __FILE__), array('jquery'), '', true );
 	wp_enqueue_script( 'jquery-ui-datepicker' );
   	wp_enqueue_script( 'field-date-js', plugins_url('js/Field_Date.js', __FILE__), array('jquery-core', 'jquery-ui-core', 'jquery-ui-datepicker'), '', true );
-	wp_enqueue_style('jquery-style', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/smoothness/jquery-ui.css');
+	wp_enqueue_style( 'jquery-style', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/smoothness/jquery-ui.css' );
   }
 }
 add_action( 'admin_enqueue_scripts', 'hrm_jobs_enqueue_scripts' );
@@ -46,13 +46,13 @@ require_once 'render-admin.php';
  * Create Sorting Admin Page
  */
 function hrm_jobs_add_menu_page() {
-	add_submenu_page( 
-		'edit.php?post_type=job', 
-		'Reorder Jobs', 
-		'Reorder Jobs', 
-		'edit_pages', 
-		'reorder_jobs', 
-		'hrm_jobs_render_admin' 
+	add_submenu_page(
+		'edit.php?post_type=job',
+		'Reorder Jobs',
+		'Reorder Jobs',
+		'edit_pages',
+		'reorder_jobs',
+		'hrm_jobs_render_admin'
 	);
 }
 
@@ -64,14 +64,16 @@ function hrm_jobs_list_shortcode ( $atts, $content = null ) {
 	$args = array(
 		'post_type' => 'job',
 		'orderby' => 'menu_order',
-		'order' => 'ASC'
+		'order' => 'ASC',
+		'post_per_page' => 100, /* add a reasonable max # rows */
+		'no_found_rows' => true, /* don't generate a count as part of query */
 	);
 
 	$jobs = new WP_Query( $args );
 	?>
-		
+
 	<?php   if ( $jobs->have_posts() ) : ?>
-	    	
+
 			<table id="job-list">
 				<tr>
 					<th>Job Title</th>
@@ -79,20 +81,20 @@ function hrm_jobs_list_shortcode ( $atts, $content = null ) {
 					<th></th>
 				</tr>
 				<?php while ( $jobs->have_posts() ) : $jobs->the_post(); ?>
-					
+
 					<?php $jobUrl = get_permalink(); ?>
 
 				<tr>
 					<td id="<?php the_id(); ?>"><?php the_title(); ?></td>
 					<td><?php the_terms( $post->ID, 'location') ?></td>
-					<td><a href="<?php echo esc_url($jobUrl) ?>">Learn More</a></td>
+					<td><a href="<?php echo esc_url( $jobUrl ) ?>">Learn More</a></td>
 				</tr>
-					
+
 				<?php endwhile; ?>
-	    	</table>
+			</table>
 
 			<?php else: ?>
-			<p><?php _e('You have no Jobs to display.', 'hrm_jobs'); ?></p>
+			<p><?php _e( 'You have no Jobs to display.', 'hrm_jobs' ); ?></p>
 			<?php endif; ?>
 		</div>
 
