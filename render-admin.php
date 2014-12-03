2a6f5c928d8296f78d3d2a2f94ccd5486e3854e0
Bobby Bryant <bobby@hatrackmedia.com>
2014-12-03 00:01:52 -0500

    Improved capability check

--- render-admin.php
+++ render-admin.php
@@ -27,18 +27,19 @@ function hrm_jobs_save_order() {
 
 	//@todo add a nonce here to verify user intent
 	//capability check to ensure use caps
-	if ( current_user_can( 'manage_options') ) {
-		global $wpdb; // WordPress database class
-			$order = explode(',', $_POST['order']);
-			$counter = 0;
-			foreach ($order as $item_id) {
-				$wpdb->update( $wpdb->posts, array( 'menu_order' => $counter ), array( 'ID' => (int) $item_id) );
-				$counter++;
-			}
-			die(1);
-	} else {
-		echo esc_html( 'you cannot do this' ); exit;
+	if ( ! current_user_can( 'manage_options' ) ) {
+		wp_die(__( 'You do not have permission to access this page.' ) );
 	}
+		
+	global $wpdb; // WordPress database class
+		$order = explode(',', $_POST['order']);
+		$counter = 0;
+		foreach ($order as $item_id) {
+			$wpdb->update( $wpdb->posts, array( 'menu_order' => $counter ), array( 'ID' => (int) $item_id) );
+			$counter++;
+		}
+		die(1);
+	
 }
 
 add_action( 'wp_ajax_save_sort', 'hrm_jobs_save_order' );
